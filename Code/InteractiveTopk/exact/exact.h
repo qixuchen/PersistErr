e9a142dff343bce74729cfa2afffad5099f576e9
qixuchen <qchenax@gmail.com>
2022-10-07 16:38:40 +0800

    add code for statistic recording and printing

--- Code/InteractiveTopk/exact/exact.h
+++ Code/InteractiveTopk/exact/exact.h
@@ -5,8 +5,10 @@
 #ifndef EXACT_ALG
 #define EXACT_ALG
 
-
-
+extern double best_score;
+extern int correct_count;
+extern double question_num;
+extern double return_size;
 struct Region
 {
 	Region() = default;
